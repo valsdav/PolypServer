19328efef58e34590f1c909f458212dc949274f7
Davide Valsecchi <davide.vals94@gmail.com>
2012-07-26 11:01:37 +0200

    ramo dell'applicazione versione java1.6

--- src/it/valsecchi/polypserver/exception/UserAlreadyConnectedException.java
+++ src/it/valsecchi/polypserver/exception/UserAlreadyConnectedException.java
@@ -2,4 +2,9 @@ package it.valsecchi.polypserver.exception;
 
 public class UserAlreadyConnectedException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2007059883462839443L;
+
 }
